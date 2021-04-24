a14e9a14dbed818afae3bb783d8c7e6b494c6795
Your Name <you@example.com>
2021-04-24 18:47:03 +0200

    ToDoItem class refactor

--- src/to_do_storage.py
+++ src/to_do_storage.py
@@ -7,9 +7,6 @@ class ToDoItem:
     Storage of one single work. Include name, deadline, and importance.
     Two items are the same if they have simmilar name. (This will be probably changed in the future)
     """
-    __title: str
-    __deadline: datetime.date
-    __importance: int
 
     def __init__(self, title, deadline, importance):
         self.__title = title
